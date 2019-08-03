75c78acf130eb4f854a4fb64b645b6554081f700
Trick <trick.developer@gmail.com>
2019-08-03 11:33:23 -0400

    reformat, minor tweaks

--- src/DateExpander.php
+++ src/DateExpander.php
@@ -6,7 +6,6 @@ use Carbon\Carbon;
 
 class DateExpander
 {
-    /** @var array */
     public const EXPANDED_DATE_TYPES = [
         'age',
         'day',
@@ -27,6 +26,86 @@ class DateExpander
 
     public $timeFormat = 'g:i A';
 
+
+    /**
+     * Filters array $items and returns only the key/value pairs that exist in $types (array of key names)
+     *
+     * @param $items
+     * @param $types
+     *
+     * @return object
+     */
+    public function filterTypesByKey($items, $types)
+    {
+        return array_filter($items, static function ($key) use ($types) {
+            return in_array($key, $types, true);
+        }, ARRAY_FILTER_USE_KEY);
+    }
+
+    /**
+     * Creates complete expanded age object data, returned as an array
+     *
+     * @param \Carbon\Carbon $date
+     *
+     * @return array
+     */
+    public function getFullExpandedAgeArray(Carbon $date)
+    {
+        $now = Carbon::now();
+        $ageSeconds = $now->diffInSeconds($date, false) * (-1);
+
+        return [
+            'seconds'=>$ageSeconds,
+            'minutes'=>(int)floor($ageSeconds / 60),
+            'hours'=>(int)floor((int)floor($ageSeconds / 60) / 60),
+            'days'=>$now->diffInDays($date) * (-1),
+            'weeks'=>$now->diffInWeeks($date) * (-1),
+        ];
+    }
+
+    /**
+     * Creates complete expanded date object data, returned as an array
+     *
+     * @param \Carbon\Carbon $date
+     *
+     * @return array
+     */
+    public function getFullExpandedDateArray(Carbon $date)
+    {
+        return [
+            'value'=>$date->toIso8601String(),
+            'relative'=>$date->diffForHumans(),
+            'formatted'=>$date->toFormattedDateString(),
+            'time'=>$date->format($this->timeFormat),
+            'timestamp'=>$date->timestamp,
+            'day'=>$date->dayName,
+            'age'=>$this->createExpandedAgeObject($date),
+        ];
+    }
+
+    public function getFilteredExpandedDateArray($date, $types = self::EXPANDED_DATE_TYPES)
+    {
+        return $this->filterTypesByKey(
+            $this->getFullExpandedDateArray($date),
+            $types
+        );
+    }
+
+    /**
+     * Create an expanded date object
+     *
+     * @param \Carbon\Carbon $date
+     * @param array $types
+     *
+     * @return object
+     */
+    protected function createExpandedDateObject(Carbon $date, array $types = self::EXPANDED_DATE_TYPES)
+    {
+        return ExpandedDate::assign(
+            $this->getFilteredExpandedDateArray($date, $types)
+        );
+    }
+
     /**
      * Generates an object representing an 'age' as seconds, minutes, hours, days, and weeks.
      *
@@ -37,43 +116,14 @@ class DateExpander
      *      $now < $date ($date->isFuture())
      *
      * @param \Carbon\Carbon|string $date
+     *
      * @return object
      */
     protected function createExpandedAgeObject($date, array $types = self::EXPANDED_AGE_TYPES)
     {
-        $now = Carbon::now();
-        $ageSeconds = $now->diffInSeconds($date, false) * (- 1);
-
-        $result = [
-            'seconds' => $ageSeconds,
-            'minutes' => (int)floor($ageSeconds / 60),
-            'hours' => (int)floor((int)floor($ageSeconds / 60) / 60),
-            'days' => $now->diffInDays($date) * (- 1),
-            'weeks' => $now->diffInWeeks($date) * (- 1),
-        ];
-
-        return (object)array_filter($result, function ($key) use ($types) {
-            return in_array($key, $types, true);
-        }, ARRAY_FILTER_USE_KEY);
-    }
-
-    protected function createExpandedDateObject(Carbon $date, array $types = self::EXPANDED_DATE_TYPES)
-    {
-        $result = [
-            'value' =>  $date->toIso8601String(),
-            'relative' => $date->diffForHumans(),
-            'formatted' => $date->toFormattedDateString(),
-            'time' => $date->format($this->timeFormat),
-            'timestamp' => $date->timestamp,
-            'day' => $date->dayName,
-            'age' => $this->createExpandedAgeObject($date, ['hours','seconds']),
-        ];
-
-        return ExpandedDate::assign(
-            array_filter($result, function ($key) use ($types) {
-                    return in_array($key, $types, true);
-                }, ARRAY_FILTER_USE_KEY
-            )
+        return (object)$this->filterTypesByKey(
+            $this->getFullExpandedAgeArray($date),
+            $types
         );
     }
 
@@ -88,9 +138,10 @@ class DateExpander
      * You can pass a Eloquent Model's created_at/updated_at properties directly
      * in as the $date parameter.
      *
-     * @param Model $model
+     * @param Model                 $model
      * @param \Carbon\Carbon|string $date
-     * @param string $conditional
+     * @param string                $conditional
+     *
      * @return ExpandedDate
      */
     public function expand($date): object
@@ -101,12 +152,10 @@ class DateExpander
 
         return $this->createExpandedDateObject($date);
     }
-
 }
-
 /*
 include __DIR__.'/../vendor/autoload.php';
-$de = new DateExpander();
-$ed1 = $de->expand(Carbon::now()->subSeconds(35));
-print_r($ed1);
-*/
+//$de = new DateExpander();
+//$ed1 = $de->expand(Carbon::now()->subSeconds(35));
+$ed1 = ExpandedDate::create(Carbon::now()->subSeconds(35));
+print_r($ed1);*/
--- src/ExpandedDate.php
+++ src/ExpandedDate.php
@@ -3,26 +3,40 @@
 namespace Permafrost\ExpandedDates;
 
 /**
- * Class ExpandedDate
- * @package Permafrost\ExpandedDates
+ * Class ExpandedDate.
  *
- * @property-read string $value
- * @property-read string $relative
- * @property-read string $timezone
- * @property-read string $formatted
- * @property-read string $time
- * @property-read string $age
+ * @property string $value
+ * @property string $relative
+ * @property string $timezone
+ * @property string $formatted
+ * @property string $time
+ * @property string $age
  */
 class ExpandedDate extends \stdClass
 {
+    /**
+     * Assigns all key/value pairs in $props as properties on the returned object.
+     *
+     * @param array $props
+     *
+     * @return \Permafrost\ExpandedDates\ExpandedDate
+     */
     public static function assign(array $props)
     {
-        $result = new static;
+        $result = new static();
 
-        foreach($props as $key => $value) {
+        foreach ($props as $key=>$value) {
             $result->$key = $value;
         }
 
         return $result;
+    }
+
+    public static function create($date)
+    {
+        $expander = new DateExpander();
+        return static::assign(
+            $expander->getFilteredExpandedDateArray($date)
+        );
     }
 }
